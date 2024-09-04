a9f9e83db502925401da05f900d0e7dee03d9d03
jcha0155 <jcha0155@student.monash.edu>
2024-09-04 01:58:18 +0000

    Pushing an update

--- src/wavmark/utils/wm_add_util.py
+++ src/wavmark/utils/wm_add_util.py
@@ -72,9 +72,10 @@ def encode_trunck_with_snr_check(idx_trunck, signal, wm, device, model, min_snr,
         encode_times += 1
         signal_wmd = encode_trunck(signal_for_encode, wm, device, model)
         snr = metric_util.signal_noise_ratio(signal, signal_wmd)
-        if encode_times == 1 and snr < min_snr:
-            print("test subject")
-            print("skip section:%d, snr too low:%.1f" % (idx_trunck, min_snr))
+        #if encode_times == 1 and snr < min_snr:
+        if encode_times == 1:
+            print("test subject 1")
+            #print("skip section:%d, snr too low:%.1f" % (idx_trunck, min_snr))
             return signal, "skip"
 
         if snr < max_snr:
